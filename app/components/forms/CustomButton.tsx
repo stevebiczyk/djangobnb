523ea649ab4ac8f3a29a3400d5b28942d793b8b4
stevebiczyk <stevebiczyk@gmail.com>
2025-07-13 16:06:20 +0100

    added ConversationDetail component

---
+++ app/components/forms/CustomButton.tsx
@@ -0,0 +1,21 @@
+interface CustomButtonProps {
+  className?: string;
+  label: string;
+  onClick: () => void;
+}
+
+const CustomButton: React.FC<CustomButtonProps> = ({
+  label,
+  className,
+  onClick,
+}) => {
+  return (
+    <div
+      onClick={onClick}
+      className={`w-full py-4 bg-airbnb hover:bg-airbnb-dark text-white text-center rounded-xl transition cursor-pointer ${className}`}
+    >
+      {label}
+    </div>
+  );
+};
+export default CustomButton;
---
+++ app/components/inbox/ConversationDetail.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import CustomButton from "../forms/CustomButton";
+
+const ConversationDetail = () => {
+  return (
+    <>
+      <div className="max-h-[400px] overflow-auto flex flex-col space-y-4">
+        <div className="w-[80%] py-4 px-6 rounded-xl bg-gray-200">
+          <p className="font-bold text-gray-800">John Doe</p>
+          <p className="text-gray-600">Hello! How are you?</p>
+        </div>
+        <div className="w-[80%] ml-[20%] py-4 px-6 rounded-xl bg-blue-200">
+          <p className="font-bold text-gray-800">John Doe</p>
+          <p className="text-gray-600">Hello! How are you?</p>
+        </div>
+      </div>
+      <div className="mt-4 py-4 px-6 flex border border-gray-300 space-x-4 rounded-xl">
+        <input
+          type="text"
+          placeholder="Type your message..."
+          className="w-full p-2 bg-gray-200 rounded-xl"
+        />
+        <CustomButton
+          label="Send..."
+          onClick={() => console.log("Button Clicked")}
+          className="w-[100px]"
+        />
+      </div>
+    </>
+  );
+};
+export default ConversationDetail;
