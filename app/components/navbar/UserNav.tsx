8beb24f42f63246e3fead4db8aa8bc1153b4b584
stevebiczyk <stevebiczyk@gmail.com>
2025-07-14 22:42:13 +0100

    added User Mnu popup

--- app/components/navbar/UserNav.tsx
+++ app/components/navbar/UserNav.tsx
@@ -1,7 +1,14 @@
+"use client";
+
+import { useState } from "react";
+import MenuLink from "./MenuLink";
+
 const UserNav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="p-2 relative inline-block border rounded-full">
-      <button className="flex items-center">
+      <button onClick={() => setIsOpen(!isOpen)} className="flex items-center">
         <svg
           fill="none"
           viewBox="0 0 24 24"
@@ -30,6 +37,23 @@ const UserNav = () => {
           />
         </svg>
       </button>
+
+      {isOpen && (
+        <div className="w-[220px] absolute top-[60px] right-0 bg-white border rounded-xl shadow-md flex flex-col cursor-pointer">
+          <MenuLink
+            label="Log In"
+            onClick={() => console.log("Button Clicked")}
+          />
+          <MenuLink
+            label="Sign Up"
+            onClick={() => console.log("Button Clicked")}
+          />
+          <MenuLink
+            label="Profile"
+            onClick={() => console.log("Button Clicked")}
+          />
+        </div>
+      )}
     </div>
   );
 };
