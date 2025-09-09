1ed820700122d0e108021243e6a0b9d38dd374e1
stevebiczyk <stevebiczyk@gmail.com>
2025-09-09 21:47:05 +0100

    added steps 2-4 tp addPropertyModal

--- app/components/modals/AddPropertyModal.tsx
+++ app/components/modals/AddPropertyModal.tsx
@@ -10,8 +10,17 @@ import CustomButton from "../forms/CustomButton";
 const AddPropertyModal = () => {
   //
   // States
-  const [datacategory, setDataCategory] = useState("");
   const [currentStep, setCurrentStep] = useState(1);
+  const [datacategory, setDataCategory] = useState("");
+  const [dataTitle, setDataTitle] = useState("");
+  const [dataDescription, setDataDescription] = useState("");
+  const [dataPrice, setDataPrice] = useState("");
+  const [dataBedrooms, setDataBedrooms] = useState("");
+  const [dataBathrooms, setDataBathrooms] = useState("");
+  const [dataGuests, setDataGuests] = useState("");
+
+  //
+  // Hooks
   const addPropertyModal = useAddPropertyModal();
   //
   //
@@ -30,8 +39,106 @@ const AddPropertyModal = () => {
           />
           <CustomButton label="Next" onClick={() => setCurrentStep(2)} />
         </>
+      ) : currentStep == 2 ? (
+        <>
+          <h2 className="mb-6 text-2xl">Describe your place</h2>
+
+          <div className="pt-3 pb-6 space-y-4">
+            <div className="flex flex-col space-y-2">
+              <label>Title</label>
+              <input
+                type="text"
+                value={dataTitle}
+                onChange={(e) => setDataTitle(e.target.value)}
+                className="w-full p-4 border border-gray-600 rounded-xl"
+              />
+            </div>
+
+            <div className="flex flex-col space-y-2">
+              <label>Description</label>
+              <textarea
+                value={dataDescription}
+                onChange={(e) => setDataDescription(e.target.value)}
+                className="w-full h-[200px] p-4 border border-gray-600 rounded-xl"
+              ></textarea>
+            </div>
+          </div>
+
+          <CustomButton
+            label="Previous"
+            className="mb-2 bg-black hover:bg-gray-800"
+            onClick={() => setCurrentStep(1)}
+          />
+          <CustomButton label="Next" onClick={() => setCurrentStep(3)} />
+        </>
+      ) : currentStep == 3 ? (
+        <>
+          <h2 className="mb-6 text-2xl">Details</h2>
+
+          <div className="pt-3 pb-6 space-y-4">
+            <div className="flex flex-col space-y-2">
+              <label>Price per night</label>
+              <input
+                type="number"
+                value={dataPrice}
+                onChange={(e) => setDataPrice(e.target.value)}
+                className="w-full p-4 border border-gray-600 rounded-xl"
+              />
+            </div>
+
+            <div className="flex flex-col space-y-2">
+              <label>Number of guests</label>
+              <input
+                type="number"
+                value={dataGuests}
+                onChange={(e) => setDataGuests(e.target.value)}
+                className="w-full p-4 border border-gray-600 rounded-xl"
+              />
+            </div>
+
+            <div className="flex flex-col space-y-2">
+              <label>Number of bedrooms</label>
+              <input
+                type="number"
+                value={dataBedrooms}
+                onChange={(e) => setDataBedrooms(e.target.value)}
+                className="w-full p-4 border border-gray-600 rounded-xl"
+              />
+            </div>
+
+            <div className="flex flex-col space-y-2">
+              <label>Number of bathrooms</label>
+              <input
+                type="number"
+                value={dataBathrooms}
+                onChange={(e) => setDataBathrooms(e.target.value)}
+                className="w-full p-4 border border-gray-600 rounded-xl"
+              />
+            </div>
+          </div>
+
+          <CustomButton
+            label="Previous"
+            className="mb-2 bg-black hover:bg-gray-800"
+            onClick={() => setCurrentStep(2)}
+          />
+          <CustomButton label="Next" onClick={() => setCurrentStep(4)} />
+        </>
+      ) : currentStep == 4 ? (
+        <>
+          <h2 className="mb-6 text-2xl">Location</h2>
+
+          <div className="pt-3 pb-6 space-y-4">Select Country</div>
+
+          <CustomButton
+            label="Previous"
+            className="mb-2 bg-black hover:bg-gray-800"
+            onClick={() => setCurrentStep(3)}
+          />
+          <CustomButton label="Next" onClick={() => setCurrentStep(5)} />
+        </>
       ) : (
-        <p>Step 2</p>
+        <p>Step </p>
       )}
     </>
   );
